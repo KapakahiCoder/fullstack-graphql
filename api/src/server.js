7c42b639b84b46d39b80c02afdd499c5b80c5568
Kapakahi <rubbishman@gmail.com>
2020-09-05 20:32:26 +0900

    setting up resolvers

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,14 @@ const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const {models, db} = require('./db')
 
-const server = new ApolloServer()
+const server = new ApolloServer({
+  context() {
+    // whatever you return below is the "context" and you should pass models
+    // so that you can access it inside your resovlers or you can just imports models 
+    // on top of the resolvers file.
+    return {models}
+  }
+})
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
